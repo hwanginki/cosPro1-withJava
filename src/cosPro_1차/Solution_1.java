9671df64a3459264ebf3124191d993451a866c41
every3743 <every3743@gmail.com>
2022-01-07 09:35:32 +0900

    1급 1차_1문제

--- src/cosPro_1차/Solution_1.java
+++ src/cosPro_1차/Solution_1.java
@@ -1,15 +1,19 @@
 package cosPro_1차;
 
-class Solution {
+class Solution_1 {
     public long solution(long num) {
-        // Write code here.
-        long answer = 0;
-        return answer;
+    	num++;
+    	long digit = 1;
+    	while (num / digit % 10 == 0) {
+    		num += digit;
+    		digit *= 10;
+    	}
+    	return num;
     }
 
     // The following is main method to output testcase.
     public static void main(String[] args) {
-        Solution sol = new Solution();
+    	Solution_1 sol = new Solution_1();
         long num = 9949999;
         long ret = sol.solution(num);
 
